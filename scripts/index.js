e6f3a206068f69afb9b5d4eb8d752e27ab4a38ee
Manjunath Nayak <44097434+ManjunathNayak2k@users.noreply.github.com>
2020-08-11 16:28:15 +0530

    Removed test Code

--- scripts/index.js
+++ scripts/index.js
@@ -55,21 +55,3 @@ function printer(param) {
         console.log(param[i]);
     }
 }
-// 01/04/20, 12:00 pm - Mridul Mohta RV CS changed the group description
-// 01/04/20, 11:41 am - Mridul Mohta RV CS: That's my boy
-// 01/04/20, 11:51 am - Manoj Rvce: <Media omitted>
-// 01/04/20, 11:51 am - Manjunath S Nayak: Who's this
-// 17/03/20, 7:05 pm - Mridul Mohta RV CS: I'm disappointed
-// 17/03/20, 7:05 pm - Manoj Rvce: 😂😂😂😂
-// 17/03/20, 7:09 pm - Mridul Mohta RV CS: True
-// 17/03/20, 7:10 pm - Amaan RV CS: 💯
-// 01/04/20, 11:52 am - Manjunath S Nayak: This is more important
-// 17/03/20, 7:08 pm - Amaan RV CS: This message was deleted
-// 17/03/20, 7:08 pm - Mridul Mohta RV CS: We support you mate
-// 01/04/20, 11:52 am - NEW1: This is more important
-// 17/03/20, 7:08 pm - NEW2: This message was deleted
-// 17/03/20, 7:08 pm - NEW3: We support you mate
-// 17/03/20, 7:08 pm - Mridul Mohta RV CS: We support you mate
-// 01/04/20, 11:52 am - NEW4: This is more important
-// 17/03/20, 7:08 pm - NEW5: This message was deleted
-// 17/03/20, 7:08 pm - NEW6: We support you mate
